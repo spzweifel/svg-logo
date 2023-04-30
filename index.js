090e217ef6f2629c63cd8bce42f55521411552e4
Sean Zweifel <spzweifel@gmail.com>
2023-04-30 19:14:19 -0400

    did a lot. 90% done

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const inquirer = require("inquirer");
-const {Triangle, Square, Circle} = require("./lib/shape")
+const { Triangle, Square, Circle } = require("./lib/shape");
 const fs = require("fs");
-class Logo {
-  constructor() {
-    this.text = "";
-    this.shape = "";
-  }
-  setText_color(initials, color){
-    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${initials}</text>`
-  }
-  setShape(shape){
-    this.shape = shape.render();
-  }
-  render(){
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.text}${this.shape}</scg>`
-  }
-}
+// class Logo {
+//   constructor() {
+//     this.text = "";
+//     this.shape = "";
+//   }
+//   setText_color(initials, color) {
+//     this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${initials}</text>`;
+//   }
+//   setShape(shape) {
+//     this.shape = shape.render();
+//   }
+//   render() {
+//     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.text}${this.shape}</scg>`;
+//   }
+// }
 
 const questions = () => {
-  return inquirer.prompt([
+  return inquirer
+    .prompt([
       {
         // here, make it so only three letters can be entered.
         //look up validate in inquirer documentation
@@ -47,17 +48,48 @@ const questions = () => {
     ])
     .then((answers) => {
       console.log(answers);
-      fs.writeFile("logo.svg", answers, (err) => 
-      err ? console.log(err) : console.log("logo created")
-      )
+      let shape;
+      switch (answers.shape) {
+        case "circle":
+          shape = new Circle(answers.shape_color);
+          break;
+        case "triangle":
+          shape = new Triangle(answers.shape_color);
+          break;
+        case "square":
+          shape = new Square(answers.shape_color);
+          break;
+      }
+      const solution = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render(answers.shape_color)}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${
+          answers.text_color
+        }">${answers.initials}</text>
+      </svg>`;
+      const name = "logo.svg";
+      writeFile(name, solution);
+      return solution;
     });
 };
 
+//     function genTemplate(Triangle, Square, Circle) {
+//       answers.text =
+//     }
+// };
 
+function writeFile(name, solution) {
+  fs.writeFile(name, solution, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("logo created");
+    }
+  });
+}
 
 questions();
 
-
-
 //jest is installed. Should run code using jest
 
+// ask jaime why this isn't working.
+// ${shape.render(answers.shape_color)}
